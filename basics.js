fc9604f21ab1dcd5b45e03a7a180ab21604a9540
Hit-magrath <hitmagrath@gmail.com>
2024-11-16 11:51:42 +0200

    hide and show

--- basics.js
+++ basics.js
@@ -3,5 +3,9 @@
         <h2> Hi, welcome to my first javascript poject</h2>
         <p id = 'thing'> click the button below</p>
         <button type="button" onclick = "document.getElementById('thing').innerHTML = 'Hello world!'"> click here</button>
+        <p id= "hidden" style = "display:none">I can show hidden texts</p>
+        <button type ="button" onclick = "document.getElementById('hidden').style.display = 'block' "> show hidden</button>
+        <p id = "other"> i can hide</p>
+        <button type = 'button' onclick = "document.getElementById('other').style.display='none'"> hide texts</button>
     </body>
 </html>
