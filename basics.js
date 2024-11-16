c5222ed9b6638810e7a4b0e183fb8b74562194f8
Hit-magrath <hitmagrath@gmail.com>
2024-11-16 11:18:59 +0200

    initial

---
+++ basics.js
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h2> Hi, welcome to my first javascript poject</h2>
+        <p id = 'thing'> click the button below</p>
+        <button type="button" onclick = "document.getElementById('thing').innerHTML = 'Hello world!'"> click here</button>
+    </body>
+</html>
